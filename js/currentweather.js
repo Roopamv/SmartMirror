4eaf3a5c24251c8c9318a70e3adf2d3d1e150fb6
Neel Renavikar <neel.renavikar@gmail.com>
2018-04-04 11:20:36 +0530

    Update currentweather.js

--- js/currentweather.js
+++ js/currentweather.js
@@ -2,7 +2,7 @@ var self = this;
 moment.locale("en");
 //this.scheduleUpdate(0);
 
-			$.getJSON("http://api.openweathermap.org/data/2.5/weather?id=1253367&APPID=ce2fa2a16af8d4c0226329790cd0ee2c",function(json){            
+			$.getJSON("http://api.openweathermap.org/data/2.5/weather?id=1253367&APPID=<API ID>",function(json){            
 		document.getElementById("weather").innerHTML = "City :" + json.name;
 		document.getElementById("weather").innerHTML += "<br />";
 		document.getElementById("weather").innerHTML += "Weather :" + json.weather[0].main;
@@ -20,7 +20,7 @@ moment.locale("en");
 });
 
 setInterval(function(){
-			$.getJSON("http://api.openweathermap.org/data/2.5/weather?id=1253367&APPID=ce2fa2a16af8d4c0226329790cd0ee2c",function(json){            
+			$.getJSON("http://api.openweathermap.org/data/2.5/weather?id=1253367&APPID=<API ID>",function(json){            
 		document.getElementById("weather").innerHTML = "City :" + json.name;
 		document.getElementById("weather").innerHTML += "<br />";
 		document.getElementById("weather").innerHTML += "Weather :" + json.weather[0].main;
